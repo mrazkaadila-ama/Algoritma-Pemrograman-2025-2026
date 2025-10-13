bbb91e53a730a007c9a157bda4a58bb166365de6
Muhamad Razka Adila <mrazkaadila@gmail.com>
2025-10-13 13:38:42 +0700

    Create tugas2.java

---
+++ java/tugas2.java
@@ -0,0 +1,11 @@
+importjava.util.Scanner;
+public class Main{
+    public static vois main(Sring[]args){
+        final double Pi= 3.14159;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan jari-jari:");
+        double r = sc.nextDouble();
+        double luas = PI*r*r;
+        System.outprintln("Luas lingkaran ="+luas);
+    }
+    }
