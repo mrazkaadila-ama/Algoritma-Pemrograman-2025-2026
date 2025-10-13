26bd4d8ad0d348cb74a2b5f41e70cbcfe16591e6
Muhamad Razka Adila <mrazkaadila@gmail.com>
2025-10-13 13:38:50 +0700

    Create tugas1.java

---
+++ java/tugas1.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[]args){
+        String nama="Muhamad Razka Adila";
+        int umur=18;
+        double tinggi=165;
+        boolean aktif=true;
+
+        system.out.println("Nama:"+nama);
+        system.out.println("Umur:"+umur);
+        system.out.println("Tinggi:"+tinggi);
+        system.out.println("Status aktif:"+aktif);
+    }   
+}
