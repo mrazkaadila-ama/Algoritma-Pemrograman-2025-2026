bbdff7fefd7b84f9252371d6f12812c02bd6f3f6
Muhamad Razka Adila <mrazkaadila@gmail.com>
2025-10-13 13:38:08 +0700

    Create tugas1.cpp

---
+++ c atau c++/tugas1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string nama ="Muhamad Razka Adila";
+    int umur = 18;
+    float tinggi = 165;
+    bool aktif = true;
+
+    cout<<"Nama:"<<nama<<endl;
+    cout<<"Umur:"<<umur<<endl;
+    cout<<"Tinggi:"<<tinggi<<endl;
+    cout<<"Status aktif:"<<aktif<<endl;
+    return 0;
+}
+
+    
+
