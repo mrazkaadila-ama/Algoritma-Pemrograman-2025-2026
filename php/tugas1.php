e4ac732652a08dfb17ffec5a77c42822d2ebff5d
Muhamad Razka Adila <mrazkaadila@gmail.com>
2025-10-13 13:38:39 +0700

    Create tugas1.php

---
+++ php/tugas1.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Muhamad Razka Adila";
+$umur = 18;
+$tinggi = 165;
+$aktif = true;
+
+echo "Nama:$nama<br>";
+echo "Umur:$umur<br>";
+echo "Tinggi:$tinggi<br>";
+echo "Status aktif:$aktif";
+?>
