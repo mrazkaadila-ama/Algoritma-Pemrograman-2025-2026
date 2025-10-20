ddef1d52a879fd9bd2c1d56908363a9f1a3e8f16
Muhamad Razka Adila <mrazkaadila@gmail.com>
2025-10-20 19:22:10 +0700

    Create Floating.java

---
+++ java/Floating.java
@@ -0,0 +1,9 @@
+public class Floating {
+    public static void main(String[] args){
+        float short_pi = 3.14f;
+        double long_pi = 3.1415926535899793d;
+
+        System.out.println("Nilai short pi =" + short_pi);
+        System.out.println("Nilai long pi =" + long_pi);
+    }
+}
