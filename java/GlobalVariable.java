27884bc02fe91ef4ce356b90475963b496a7b84e
Muhamad Razka Adila <mrazkaadila@gmail.com>
2025-10-20 19:22:05 +0700

    Create GlobalVariable.java

---
+++ java/GlobalVariable.java
@@ -0,0 +1,8 @@
+public class GlobalVariable {
+    public static int age;
+
+    public static void main(String[] args) {
+        age = 18;
+        System.out.println("Umur Muhamad Razka Adila : " + age);
+    }
+}
