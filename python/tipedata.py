80cc3fd5efe22babbd60fdfff65aaff21e11326b
Muhamad Razka Adila <mrazkaadila@gmail.com>
2025-10-20 11:07:16 +0700

    Create tipedata.py

---
+++ python/tipedata.py
@@ -0,0 +1,17 @@
+print('---Belajar Tipe Data---') 
+y = 3 
+z = 5 + 3 
+print(z) 
+a = 7.5 
+print(a) 
+myFirstName = 'Muhammad'
+myLastName = "Encep" 
+myAge = "36" 
+myHobby = """Hobi saya adalah Travelling. """  
+print(myFirstName + " " + myLastName) 
+print("Umur saya " + myAge) 
+print("Hobby saya " + myHobby) 
+a = True 
+b = False 
+c = a 
+print(c) 
