8637c9b13d1eac1091b812dd2288ae4ad0d68d1c
Muhamad Razka Adila <mrazkaadila@gmail.com>
2026-01-25 22:04:49 +0700

    Create percabangan.php

---
+++ php/percabangan.php
@@ -0,0 +1,23 @@
+<?php
+$hari = 3;
+
+switch ($hari) {
+    case 1:
+        echo "Senin";
+        break;
+    case 2:
+        echo "Selasa";
+        break;
+    case 3:
+        echo "Rabu";
+        break;
+    case 4:
+        echo "Kamis";
+        break;
+    case 5:
+        echo "Jumat";
+        break;
+    default:
+        echo "Hari tidak valid";
+}
+?>
